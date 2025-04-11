39757573bc8df9808e5576a3add2c75d0f3afc42
Muhammad Qasim <mqtech25@gmail.com>
2025-04-11 16:56:53 +0500

    LP update

--- frontend/Positivus_Landing_Page_Design/script.js
+++ frontend/Positivus_Landing_Page_Design/script.js
@@ -6,3 +6,13 @@ menuBar.addEventListener('click',function(e) {
     this.classList.toggle('open')
     navList.classList.toggle('show')
 })
+
+const questionCard = document.querySelectorAll('.question_card-head');
+console.log(questionCard.length);
+
+for(let i=0; i<questionCard.length; i++){
+    questionCard[i].addEventListener('click',function(){
+        // let id =this.parentElement.getAttribute('data-questionno');
+        this.parentElement.classList.toggle('active')
+    })
+}
