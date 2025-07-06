6b6c1d81763aa224299b6d96e52d850a3fca54e4
Muhammad Qasim <mqtech25@gmail.com>
2025-07-06 23:31:25 +0500

    expend assigment complete

--- 36-weeks-remote-jobs-preparation-challenge-main/assignments/01-js/easy/expenditure-analysis.js
+++ 36-weeks-remote-jobs-preparation-challenge-main/assignments/01-js/easy/expenditure-analysis.js
@@ -12,9 +12,65 @@
 	}
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: 'Clothing',
+    itemName: 'T-Shirt',
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: 'Electronics',
+    itemName: 'Headphones',
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: 'Clothing',
+    itemName: 'Jeans',
+  },
+];
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+   let categoriesTranstions = {};
+
+   transactions.forEach((element,index) => {
+   
+    catagories = element.category;
+    catagoriesPrice = element.price;
+    if(!categoriesTranstions[catagories]){
+        categoriesTranstions[catagories] ={
+          category:catagories,
+          totalSpent:0
+        }
+    }
+  categoriesTranstions[catagories].totalSpent += catagoriesPrice;
+
+  });
+// console.log([categoriesTranstions]);
+  return Object.values(categoriesTranstions);
 }
+
+calculateTotalSpentByCategory(transactions)
 
 module.exports = calculateTotalSpentByCategory;
